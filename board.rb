00fe37f6afa0fcf7bee2c83ec018e78d5bbb6216
Dylan Richards <dylanerichards@gmail.com>
2015-04-09 23:44:26 -0400

    Test that the board contains the winning combinations

--- board.rb
+++ board.rb
@@ -1,6 +1,17 @@
 class Board
   attr_accessor :board
 
+  WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
   def initialize
     @board = Array.new(9) { Cell.new }
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,4 +14,14 @@ describe Board do
       expect(cell).to be_a Cell
     end
   end
+
+  it "contains the winning combinations" do
+    [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ].each do |combination|
+      expect(Board::WINNING_COMBINATIONS.include?(combination)).to eq true
+    end
+  end
 end
