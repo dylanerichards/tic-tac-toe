aaf58f545215b1a9ab679f358824ce8b8eca9b29
Dylan Richards <dylanerichards@gmail.com>
2015-04-09 23:34:54 -0400

    Test that cell value is blank

--- cell.rb
+++ cell.rb
@@ -1,2 +1,7 @@
 class Cell
+  attr_accessor :value
+
+  def initialize
+    @value = :blank
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,3 +1,12 @@
 require "spec_helper"
 require_relative "../cell.rb"
 
+describe Cell do
+  let(:cell) { Cell.new }
+
+  it "has a value that defaults to blank" do
+    expect(cell.value).to eq :blank
+  end
+
+end
+
